8be7be128462ae7c77c73162c92f1f0391108856
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-12 15:57:18 +0100

    widget works

--- js/widget.js
+++ js/widget.js
@@ -1,32 +1,35 @@
 import "./widget.css";
 
-
-
-
 import { Niivue } from "@niivue/niivue";
-// make an array of volumes to load
-let volumeList = [
-  { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
-];
-
-
-
 
 export function render({ model, el }) {
-	let btn = document.createElement("button");
-	btn.classList.add("anywidget_ipyniivue_vanillajs-counter-button");
-	btn.innerHTML = `count is ${model.get("value")}`;
-	btn.addEventListener("click", () => {
-		model.set("value", model.get("value") + 1);
-		model.save_changes();
-	});
-	model.on("change:value", () => {
-		btn.innerHTML = `count is ${model.get("value")}`;
-	});
-	el.innerHTML = "Hello World!";
-	el.appendChild(btn);
-	var textNode = document.createTextNode("Goodbye");
-	el.appendChild(textNode);
+  let btn = document.createElement("button");
+  btn.classList.add("anywidget_ipyniivue_vanillajs-counter-button");
+  btn.innerHTML = `count is ${model.get("value")}`;
+  btn.addEventListener("click", () => {
+    model.set("value", model.get("value") + 1);
+    model.save_changes();
+  });
+  model.on("change:value", () => {
+    btn.innerHTML = `count is ${model.get("value")}`;
+  });
+  el.innerHTML = "Hello World!";
+  el.appendChild(btn);
+  var textNode = document.createTextNode("Goodbye");
+  el.appendChild(textNode);
+
+  var divElement = document.createElement("div");
+  var canvasElement = document.createElement("canvas");
+  canvasElement.id = "gl";
+  canvasElement.width = "800";
+  canvasElement.height = "800";
+  divElement.appendChild(canvasElement);
+  el.appendChild(divElement);
+
+  let volumeList = [
+  { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
+];
+const nv = new Niivue({ isResizeCanvas: false });
+nv.attachTo("gl"); // attach to canvas with id="gl"
+nv.loadVolumes(volumeList);
 }
-
-
