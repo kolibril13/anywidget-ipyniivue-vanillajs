2f693eac7338fa37bceb665664e9bf515d2976d5
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-12 18:35:17 +0100

    canvas idea

--- js/widget.js
+++ js/widget.js
@@ -3,17 +3,19 @@ import "./widget.css";
 import { Niivue } from "@niivue/niivue";
 
 export function render({ model, el }) {
-  var divElement = document.createElement("div");
-  var canvasElement = document.createElement("canvas");
-  canvasElement.id = "gl";
-  canvasElement.width = "800";
-  canvasElement.height = "800";
-  divElement.appendChild(canvasElement);
-  el.appendChild(divElement);
+  const canvas = document.createElement("canvas");
+  canvas.id = "gl";
+  canvas.width = 800;
+  canvas.height = 800;
 
+  // Append the canvas to the element with id="app"
+  el.appendChild(canvas);
+
+  // Make an array of volumes to load
   let volumeList = [
     { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
   ];
+
   const nv = new Niivue({ isResizeCanvas: false });
   nv.attachTo("gl"); // attach to canvas with id="gl"
   nv.loadVolumes(volumeList);
