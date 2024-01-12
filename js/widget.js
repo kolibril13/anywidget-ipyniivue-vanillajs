79425a1402e8eec36bd0ba634eec38baf8b546a1
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-12 14:17:58 +0100

    figure out createTextNode logic

--- js/widget.js
+++ js/widget.js
@@ -1,5 +1,17 @@
 import "./widget.css";
 
+
+
+
+import { Niivue } from "@niivue/niivue";
+// make an array of volumes to load
+let volumeList = [
+  { url: "https://niivue.github.io/niivue-demo-images/mni152.nii.gz" },
+];
+
+
+
+
 export function render({ model, el }) {
 	let btn = document.createElement("button");
 	btn.classList.add("anywidget_ipyniivue_vanillajs-counter-button");
@@ -11,5 +23,10 @@ export function render({ model, el }) {
 	model.on("change:value", () => {
 		btn.innerHTML = `count is ${model.get("value")}`;
 	});
+	el.innerHTML = "Hello World!";
 	el.appendChild(btn);
+	var textNode = document.createTextNode("Goodbye");
+	el.appendChild(textNode);
 }
+
+
