8b12c91882ecf96ff235de9e3f29d2075182dfc2
kolibril13 <44469195+kolibril13@users.noreply.github.com>
2024-01-20 12:42:22 +0100

    convert args to list

--- js/custom_send.js
+++ js/custom_send.js
@@ -17,7 +17,6 @@ export async function render({ model, el }) {
     console.log(msg.args);
     nv.attachToCanvas(canvas);
     let funcname = msg.func;
-    let volumeList = msg.args;
-    nv[funcname](volumeList);
+    nv[funcname]( ...msg.args);
   });
 }
